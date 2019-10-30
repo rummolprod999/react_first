092f7f3907d0ff4d0ff9bd22475000d8a5548406
rummolprod999 <rummolprod999@gmail.com>
2019-10-30 13:27:12 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {class: "off", label: "Нажми"};
+
+    this.press = this.press.bind(this);
+  }
+  press(){
+    let className = (this.state.class==="off")?"on":"off";
+    this.setState({class: className});
+  }
   render() {
-    return <h1>Hello, React</h1>;
+    return <button onClick={this.press} className={this.state.class}>{this.state.label}</button>;
   }
 }
 
